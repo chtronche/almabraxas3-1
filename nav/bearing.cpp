0a4db825cde73dfabe87b6d4b966ec8efcb0b551
Ch. Tronche <ch@tronche.com>
2020-10-25 12:40:09 +0100

    Use a circular buffer and the last two meaningful points to determine bearing

--- nav/bearing.cpp
+++ nav/bearing.cpp
@@ -1,46 +1,56 @@
 #include "mbed.h"
 
 #include "nav.h"
+#include "sdlog.h"
 
-static const int bufferSize = 64; // power of two please
+static const int _bufferSize = 64; // power of two please
 
-class _CircularBuffer {
+class _CircularBuffer { // Fixed size, always full
 
   friend class _Iterator;
 
 public:
-  _CircularBuffer() { reset(); }
+  _CircularBuffer();
 
-  void reset() { _index = 0; _n = 0; }
+  void add(float lat, float lon);
 
-  void add(float lat, float lon) {
-    cell &p = _buffer[_index];
-    p.lat = lat;
-    p.lon = lon;
-    _index = (_index + 1) % bufferSize;
-    if (_n < bufferSize) ++_n;
-  }
-  
 private:
-  cell _buffer[bufferSize];
-  int _index, _n;
+  cell _buffer[_bufferSize];
+  int _index;
   
 };
 
+_CircularBuffer::_CircularBuffer() {
+  _index = 0;
+  for(int i = _bufferSize - 1; i >= 0; --i) {
+    // You're not supposed to be here, and if you are, it's ok anyway.
+    add(27.986065, 86.922623);
+  }
+}
+
+void _CircularBuffer::add(float lat, float lon) {
+  _index = (_index + 1) % _bufferSize;
+  cell &p = _buffer[_index];
+  p.lat = lat;
+  p.lon = lon;
+}
+
 class _Iterator {
 public:
-  _Iterator(const _CircularBuffer &b):_buffer(b), _pos((b._index - 1) % bufferSize), _n(_buffer._n) { }
-  bool atEnd() const { return _n; }
+  _Iterator(const _CircularBuffer &b):_buffer(b._buffer), _pos(b._index), _n(_bufferSize) { }
+  bool atEnd() const { return _n <= 0; }
 
   void prev() {
-    _pos = (_pos - 1) % bufferSize;
+    if (atEnd()) return;
+    --_pos;
     --_n;
+    if (_pos < 0) _pos = _bufferSize - 1;
   }
 
-  const cell &value() const { return _buffer._buffer[_pos]; }
+  const cell &value() const { return _buffer[_pos]; }
 
 private:
-  const _CircularBuffer &_buffer;
+  const cell *_buffer;
   int _pos;
   int _n;
 };
@@ -49,30 +59,38 @@ _CircularBuffer _buffer;
 
 uint8_t magneticHeading = 0;
 
-static void computeBearing(float lat, float lon, uint16_t *heading, bool *headingIsMagnetic) {
-  uint8_t heading_ = getMagneticHeading();
-  magneticHeading = heading_;
-  bool headingIsMagnetic_ = true;
-
+static void computeBearing_fromTracking(float lat, float lon,
+					const cell **last,
+					uint16_t *heading,
+					bool *headingIsMagnetic) {
+  if (lat > 95) return; // No fix
   _buffer.add(lat, lon);
   _Iterator i(_buffer);
-  i.prev();
-  for(; !i.atEnd(); i.prev()) {
-    const cell &c = i.value();
+  // Starts at -1, since current coords are at the end of the buffer
+  for(i.prev(); !i.atEnd(); i.prev()) {
     float distanceM, bearing;
-    distAndHeading(c.lat, c.lon, lat, lon, distanceM, bearing);
-    if (distanceM >= 20) {
-      heading_ = uint8_t(bearing * 128 / M_PI);
-      headingIsMagnetic_ = false;
-      break;
-    }
+    const cell &c = i.value();
+    *last = &c;
+    distAndHeading(c.lat, c.lon, lat, lon, distanceM, bearing);    
+    //printf("===%f\t%f\t%f\t%f\t%f\t%f\n", c.lat, c.lon, lat, lon, distanceM, bearing / M_PI * 180);
+    // Shouldn't these values be configurable ?
+    if (distanceM < 20) continue;
+    if (distanceM > 1000) return; // We're lacking some fixes (or cruising at mach 0.6...)
+    *headingIsMagnetic = false;
+    *heading = int8_t(bearing * 128 / M_PI);
+    return;
   }
-  *heading = heading_;
-  *headingIsMagnetic = headingIsMagnetic_;
 }
 
-void noFix() {
-  _buffer.reset();
+static void computeBearing(float lat, float lon, uint16_t *heading, bool *headingIsMagnetic) {
+  magneticHeading = *heading = getMagneticHeading();
+  *headingIsMagnetic = true; // We'll change that if we get heading from tracking
+  const cell *last;
+  computeBearing_fromTracking(lat, lon, &last, heading, headingIsMagnetic);
+  char buffer[128];
+  snprintf(buffer, 128, "%f\t%f\t%f\t%f\tMH=%d\t%u\t%d", last->lat, last->lon, lat, lon,
+	   magneticHeading, *heading & 0xff, *headingIsMagnetic);
+  sdlog("bearing", buffer);
 }
 
 // This is where PID should take place
