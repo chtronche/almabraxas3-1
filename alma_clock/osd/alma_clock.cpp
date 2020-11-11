c71e33efaf38ec59f45a2b9b197b90f100cc7f63
Ch. Tronche <ch@tronche.com>
2020-11-11 22:54:20 +0100

    arm satellite clock + get rid of that annoying \n at the end of the first date log

--- alma_clock/osd/alma_clock.cpp
+++ alma_clock/osd/alma_clock.cpp
@@ -1,5 +1,6 @@
 #include "alma_clock.h"
 #include "alma_math.h"
+#include "reporting.h"
 #include "sdlog.h"
 
 static uint32_t start;
@@ -15,8 +16,19 @@ void alma_clock_init() {
 
 static bool _rtc_set = false;
 
+static void trim_n(char *buffer) {
+  char *p = strchr(buffer, '\n');
+  if (!p) {
+    // Not supposed to happen in our context
+    buffer[0] = '\0';
+    return;
+  }
+  *p = '\0';
+}
+
 void alma_clock_resetClock(const char *gpsDate, const char *gpsTime) {
   if (_rtc_set) return;
+  _rtc_set = true;
 
   struct tm _tm;
   _tm.tm_sec = fast_atoi(gpsTime + 4);
@@ -33,8 +45,11 @@ void alma_clock_resetClock(const char *gpsDate, const char *gpsTime) {
   alma_clock_advance();
 
   char buffer[128];
-  snprintf(buffer, 128, "time set to %s, delta (should be zero) = %ld", ctime(&t), alma_clock - c);
+  char time_buffer[64];
+  strncpy(time_buffer, ctime(&t), 64);
+  trim_n(time_buffer);
+  snprintf(buffer, 128, "time set to %s, delta (should be zero) = %ld", time_buffer, alma_clock - c);
   sdlog("clock", buffer);
 
-  _rtc_set = true;
+  satellite_armClock(gpsDate); // Called only once by the virtue of _rtc_set
 }
