6414d725226c184f8e3acf7a1623e2d15316b566
Ch. Tronche <ch@tronche.com>
2020-11-19 21:55:11 +0100

    Bugfix & cleanup in the satellite report embryo

--- reporting/satellite.cpp
+++ reporting/satellite.cpp
@@ -11,33 +11,32 @@ static int _reportInterval = 10;
 static uint32_t _today = 0;
 
 void satellite_loop() {
-  printf("sat %ld\t%ld\t%ld\n", _today, alma_clock, _nextTime);
   if (!_today) return; // clock not armed
   if (alma_clock < _nextTime) return;
 
-  char buffer[128];
-
   int humidity_10 = -128;
   int temp_10 = -128;
-  int error = DHT22_acquisition::acquire(DHT_22_1WIRE, &humidity_10, &temp_10);
-  printf("error = %d\n", error);
+  DHT22_acquisition::acquire(DHT_22_1WIRE, &humidity_10, &temp_10);
   humidity_10 = (humidity_10 + 5) / 10;
-  temp_10 = (humidity_10 + 5) / 10;
+  temp_10 = (temp_10 + 5) / 10;
   
-  snprintf(buffer, 128, "T=%d H=%d", temp_10, humidity_10);
-  sdlog("satcom", buffer);
   // Sending takes place here
-  if (!_nextTime)
+  if (!_nextTime) {
     // First transmisstion of the day
-    NV<uint32_t>::set("UlstXmt", &_today, _today);
+    uint32_t stub = 0;
+    NV<uint32_t>::set("UlstXmt", &stub, _today);
+  }
   _nextTime = alma_clock + _reportInterval;
+
+  char buffer[128];
+  snprintf(buffer, 128, "T=%d H=%d N=%ld", temp_10, humidity_10, _nextTime);
+  sdlog("satcom", buffer);
 }
 
 // Called only once by the virtue of alma_clock/_rtc_set
 
 void satellite_armClock(const char *date) {
   _today = fast_atoi(date + 4) << 16 | fast_atoi(date + 2) << 8 | fast_atoi(date);
-  printf("date = %lx\n", _today);
   uint32_t lastXmit = NV<uint32_t>::get("UlstXmt");
   if (lastXmit < _today) {
     _nextTime = 0; // That is now, first message of day
